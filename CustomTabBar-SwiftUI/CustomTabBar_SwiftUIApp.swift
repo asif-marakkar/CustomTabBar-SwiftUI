5ab6ef908ce5a56ed0c027679b7a30b869e98f1c
Asif Marakkar <machoasif@zoho.com>
2022-06-21 15:43:01 +0530

    Initial Commit

---
+++ CustomTabBar-SwiftUI/CustomTabBar_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBar_SwiftUIApp.swift
+//  CustomTabBar-SwiftUI
+//
+//  Created by ASIF on 21/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBar_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
